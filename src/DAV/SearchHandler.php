6d421fc4ec9d14e317de6efef06893c28a9c1fba
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2018-03-22 16:04:24 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/DAV/SearchHandler.php
+++ src/DAV/SearchHandler.php
@@ -100,7 +100,7 @@ class SearchHandler {
 	 * @return Query
 	 */
 	private function getQueryForXML(BasicSearch $xml, array $allProps) {
-		$orderBy = array_map(function (\SearchDAV\XML\Order $order) use ($allProps) {
+		$orderBy = array_map(function(\SearchDAV\XML\Order $order) use ($allProps) {
 			if (!isset($allProps[$order->property])) {
 				throw new BadRequest('requested order by property is not a valid property for this scope');
 			}
@@ -110,7 +110,7 @@ class SearchHandler {
 			}
 			return new Order($prop, $order->order);
 		}, $xml->orderBy);
-		$select = array_map(function ($propName) use ($allProps) {
+		$select = array_map(function($propName) use ($allProps) {
 			if (!isset($allProps[$propName])) {
 				return;
 			}
@@ -128,7 +128,7 @@ class SearchHandler {
 	}
 
 	private function transformOperator(\SearchDAV\XML\Operator $operator, array $allProps) {
-		$arguments = array_map(function ($argument) use ($allProps) {
+		$arguments = array_map(function($argument) use ($allProps) {
 			if (is_string($argument)) {
 				if (!isset($allProps[$argument])) {
 					throw new BadRequest('requested search property is not a valid property for this scope');
